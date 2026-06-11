2d3582aea6458acbfc464bc7dd0e42cf50efc796
Eyeru <eyeru.yeshewa20@gmail.com>
2026-06-11 08:50:08 +0300

    Add environment variable configuration

--- app/core/config.py
+++ app/core/config.py
@@ -3,12 +3,9 @@ import os
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-SECRET_KEY = os.getenv("SECRET_KEY")
-
-ALGORITHM = os.getenv("ALGORITHM")
-
+DATABASE_URL = os.environ["DATABASE_URL"]
+SECRET_KEY = os.environ["SECRET_KEY"]
+ALGORITHM = os.environ["ALGORITHM"]
 ACCESS_TOKEN_EXPIRE_MINUTES = int(
-    os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30)
+    os.environ["ACCESS_TOKEN_EXPIRE_MINUTES"]
 )
